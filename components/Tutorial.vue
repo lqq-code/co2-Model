a964e82a0f07816b5bd57d9837d5e854fa031bbc
lqq <lqq@simplechain.com>
2021-10-07 21:36:38 +0800

    site1-site6

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -3,14 +3,74 @@
     <img src="../assets/images/bg.png" class="bg_img" >
       <el-popover
         placement="top-start"
-        title="Gym"
+        title="Gym (The first floor of the Beaurepaire Centre)"
         width="380"
         trigger="hover"
         content="The first floor of the Beaurepaire Centre.">
-        <div style="width:350px;height:200px;margin:auto">
+        <div class="siteimg">
         <img src="../assets/images/site1.png"  style="width:100%;height:100%;object-fit: cover;" />
         </div>
         <el-button icon="el-icon-location" slot="reference" class="site1_button"></el-button>
+      </el-popover>
+
+      <el-popover
+        placement="top-start"
+        title="David Derham Lecture Theatre PAR 106"
+        width="380"
+        trigger="hover"
+        content="The first floor of the Beaurepaire Centre.">
+        <div class="siteimg">
+        <img src="../assets/images/site2.png"  style="width:100%;height:100%;object-fit:contain;" />
+        </div>
+        <el-button icon="el-icon-location" slot="reference" class="site2_button"></el-button>
+      </el-popover>
+
+      <el-popover
+        placement="top-start"
+        title="Union House, Main Dining Room"
+        width="380"
+        trigger="hover"
+        content="The first floor of the Beaurepaire Centre.">
+        <div class="siteimg">
+        <img src="../assets/images/site3.png"  style="width:100%;height:100%;object-fit:contain;" />
+        </div>
+        <el-button icon="el-icon-location" slot="reference" class="site3_button"></el-button>
+      </el-popover>
+
+      <el-popover
+        placement="top-start"
+        title="Chemistry laboratory (Chemistry building)"
+        width="380"
+        trigger="hover"
+        content="The first floor of the Beaurepaire Centre.">
+        <div class="siteimg">
+        <img src="../assets/images/site4.png"  style="width:100%;height:100%;object-fit:contain;" />
+        </div>
+        <el-button icon="el-icon-location" slot="reference" class="site4_button"></el-button>
+      </el-popover>
+
+      <el-popover
+        placement="top-start"
+        title="Room 409 McCoy Building : PAR 200"
+        width="380"
+        trigger="hover"
+        content="The first floor of the Beaurepaire Centre.">
+        <div class="siteimg">
+        <img src="../assets/images/site5.png"  style="width:100%;height:100%;object-fit:contain;" />
+        </div>
+        <el-button icon="el-icon-location" slot="reference" class="site5_button"></el-button>
+      </el-popover>
+
+      <el-popover
+        placement="top-start"
+        title="Room 230 (Janes Office) Alan Gilbert PAR 104"
+        width="380"
+        trigger="hover"
+        content="The first floor of the Beaurepaire Centre.">
+        <div class="siteimg">
+        <img src="../assets/images/site6.png"  style="width:100%;height:100%;object-fit:contain;" />
+        </div>
+        <el-button icon="el-icon-location" slot="reference" class="site6_button"></el-button>
       </el-popover>
     </img>
   </div>
@@ -37,15 +97,56 @@ export default {
   width: 100%;
   height: 100%;
 }
+.siteimg{
+  width:350px;
+  height:200px;
+  margin:auto
+}
+
 .site1_button {
     position: absolute;
-    right: 16%;
-    top: 10%;
+    left: 32%;
+    top: 3%;
+    transform: scale(0.8);
     border: none;
 }
-.img_site1{
-    transform: scale(0.1);
-  }
+.site2_button{
+    position: absolute;
+    left: 31%;
+    bottom: 1%;
+    transform: scale(0.8);
+    border: none;
+}
+.site3_button{
+    position: absolute;
+    left: 29%;
+    top: 16%;
+    transform: scale(0.8);
+    border: none;
+}
+
+.site4_button{
+    position: absolute;
+    left: 47%;
+    top: 29%;
+    transform: scale(0.8);
+    border: none;
+}
+.site5_button{
+    position: absolute;
+    right: 17%;
+    top: 12%;
+    transform: scale(0.8);
+    border: none;
+}
+.site6_button{
+    position: absolute;
+    left: 19%;
+    bottom: 34%;
+    transform: scale(0.8);
+    border: none;
+}
+
 
 
 </style>
